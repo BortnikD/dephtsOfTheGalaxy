3842a9b26c6016f72a5a2382737a90af0cd20c8b
BortnikD <dima100421@gmail.com>
2024-10-28 22:33:48 +0300

    Добавлена плавная анимация к расширению элементов футора с помощью jquery

--- static/js/openFooterContent.js
+++ static/js/openFooterContent.js
@@ -1,7 +1,20 @@
-var footer = document.querySelector('footer');
+$(document).ready(function () {
+    // Обрабатываем событие click для каждого элемента .footer-list__item
+    $('.footer-list__item').each(function() {
+        const $item = $(this); // Сохраняем ссылку на текущий элемент
 
-document.querySelectorAll('.footer-list__item').forEach(item => {
-    item.addEventListener('click', () => {
-        item.classList.toggle('expanded');
+        // Обработчик клика для первой линии списка
+        $item.find('.li-first-line').click(function(event) {
+            // Останавливаем любую текущую анимацию и переключаем панель с эффектом слайда
+            $item.find('.footer-content').stop(true, true).slideToggle(600); // Настройте скорость, как нужно
+            
+            // Переключаем класс expanded у элемента
+            $item.toggleClass('expanded');
+        });
+
+        // Предотвращаем закрытие меню при нажатии внутри footer-content
+        $item.find('.footer-content').click(function(event) {
+            event.stopPropagation();
+        });
     });
 });
