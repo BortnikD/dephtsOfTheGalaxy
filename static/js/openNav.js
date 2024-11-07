71e96cbcc7a6047972748f885f0006bddc3e82bb
BortnikD <dima100421@gmail.com>
2024-11-08 00:27:38 +0300

    Изменен header, вернул маршрутизацию по страницам на видное место при этом спрятал в меню панель ссылки о нас и контакты, хедер также теперь имеет прозрачность в 0.7 и фиксированное положение на экране, страница космические агенства немного заполнен и придуман дизайн для продолжения верстки

--- static/js/openNav.js
+++ static/js/openNav.js
@@ -1,8 +1,10 @@
 const navLinks = document.querySelector('.nav-links');
 const navItem = document.querySelector('#explorer');
+const menuBtn = document.querySelector('#menu-button');
 
 navItem.addEventListener('click', () => {
     navLinks.classList.toggle('hidden');
+    menuBtn.classList.toggle('hidden');
     navItem.classList.toggle('deactivate');
 })
 
@@ -11,5 +13,6 @@ document.addEventListener('click', (event) => {
     if (!navItem.contains(event.target) && !navLinks.contains(event.target)) {
         navLinks.classList.add('hidden'); // Добавляем класс 'hidden'
         navItem.classList.add('deactivate'); // Убираем класс 'deactivate'
+        menuBtn.classList.remove('hidden');
     }
 });
