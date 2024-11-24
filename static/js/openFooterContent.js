affa6edd93f365b9bd81b3c1bf8a882cc766f051
BortnikD <dima100421@gmail.com>
2024-11-24 18:05:38 +0300

    Полностью избавился от использования jquery в проекте, переписал функционал открытия контента в footer'е по-своему, благодаря этому больше нету забагованных анимаций открытия (рывков), немного изменены css-стили для полноценной работы функции

--- static/js/openFooterContent.js
+++ static/js/openFooterContent.js
@@ -1,20 +1,9 @@
-$(document).ready(function () {
-    // Обрабатываем событие click для каждого элемента .footer-list__item
-    $('.footer-list__item').each(function() {
-        const $item = $(this); // Сохраняем ссылку на текущий элемент
+const footerItems = document.querySelectorAll('.footer-list__item');
 
-        // Обработчик клика для первой линии списка
-        $item.find('.li-first-line').click(function(event) {
-            // Останавливаем любую текущую анимацию и переключаем панель с эффектом слайда
-            $item.find('.footer-content').stop(true, true).slideToggle(300); // Настройте скорость, как нужно
-            
-            // Переключаем класс expanded у элемента
-            $item.toggleClass('expanded');
-        });
-
-        // Предотвращаем закрытие меню при нажатии внутри footer-content
-        $item.find('.footer-content').click(function(event) {
-            event.stopPropagation();
-        });
+footerItems.forEach(item => {
+    item.addEventListener('click', (event) => {
+        if (!event.target.closest('.footer-content')) {
+            item.classList.toggle('expanded');
+        }
     });
 });
